fe148ca5f746874b96a4eaff264bbd7f4a9e0a8e
maybe_grace <gnecarg@gmail.com>
2025-01-22 20:54:54 +0800

    rename, greet and exit

---
+++ src/main/java/Grass.java
@@ -0,0 +1,20 @@
+public class Grass {
+    public static void main(String[] args) {
+        String logo = "  ___  ____   __   ____  ____ \n"
+                    + " / __)(  _ \ / _\ / ___)/ ___) \n"
+                    + "( (_ \ )   //    \\___ \\___ \ \n"
+                    + " \___/(__\_)\_/\_/(____/(____/ \n"
+        
+        // " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
