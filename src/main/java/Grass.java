33ca97ea2f2d46df8fc4742f8b3c88041047454a
maybe_grace <gnecarg@gmail.com>
2025-01-22 21:02:53 +0800

    Echo

--- src/main/java/Grass.java
+++ src/main/java/Grass.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class Grass {
     public static void main(String[] args) {
         String logo = "  ___  ____   __   ____  ____ \n"
-                    + " / __)(  _ \ / _\ / ___)/ ___) \n"
-                    + "( (_ \ )   //    \\___ \\___ \ \n"
-                    + " \___/(__\_)\_/\_/(____/(____/ \n"
+                    + " / __)(  _ \\ / _\\ / ___)/ ___) \n"
+                    + "( (_ \\ )   //    \\\\___ \\\\___ \\ \n"
+                    + " \\___/(__\\_)\\_/\\_/(____/(____/ \n";
         
         // " ____        _        \n"
         //         + "|  _ \\ _   _| | _____ \n"
@@ -13,6 +14,14 @@ public class Grass {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = sc.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
