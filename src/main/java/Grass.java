37ad56874d5b9c8fd1b3654c14631e0929e86c83
maybe_grace <gnecarg@gmail.com>
2025-01-22 21:29:27 +0800

    Mark as Done

--- src/main/java/Grass.java
+++ src/main/java/Grass.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.lang.Integer;
 public class Grass {
     public static void main(String[] args) {
         String logo = "  ___  ____   __   ____  ____ \n"
@@ -7,26 +8,34 @@ public class Grass {
                     + "( (_ \\ )   //    \\\\___ \\\\___ \\ \n"
                     + " \\___/(__\\_)\\_/\\_/(____/(____/ \n";
         
-        // " ____        _        \n"
-        //         + "|  _ \\ _   _| | _____ \n"
-        //         + "| | | | | | | |/ / _ \\\n"
-        //         + "| |_| | |_| |   <  __/\n"
-        //         + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        ArrayList<String> inputList = new ArrayList<String>();
+        ArrayList<Task> inputList = new ArrayList<Task>();
         while (!input.equals("bye")) {
             System.out.println("____________________________________________________________");
+            System.out.println(input);
             if (input.equals("list")) {
                 for(int i = 0; i < inputList.size(); i++) {
                     System.out.println(i + 1 + ". " + inputList.get(i));
                 }
             }
+            else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                inputList.get(taskNumber - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(inputList.get(taskNumber - 1));
+            }
+            else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                inputList.get(taskNumber - 1).markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(inputList.get(taskNumber - 1));
+            }
             else {
-                inputList.add(input);
+                inputList.add(new Task(input));
                 System.out.println("added: " + input);
             }
             System.out.println("____________________________________________________________");
