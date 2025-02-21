82db289a62028d38847d2a40d6ca56ba6e159863
maybe_grace <gnecarg@gmail.com>
2025-02-21 20:44:34 +0800

    update user guide and add title to Ui

--- src/main/java/grass/Main.java
+++ src/main/java/grass/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Grass");
             fxmlLoader.<MainWindow>getController().setGrass(grass);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
